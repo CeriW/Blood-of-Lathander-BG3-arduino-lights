5c880366d1d090c61036d629451d63525ade0d17
Ceri <cxv712@gmail.com>
2025-06-29 22:19:01 +0100

    Random white and warm with group fade

--- blood-of-lathander-lights.ino
+++ blood-of-lathander-lights.ino
@@ -10,13 +10,14 @@ struct RGB {
   uint8_t g;
   uint8_t b;
 };
+
 RGB warmYellow = { 255, 125, 0 };
 RGB coolYellow = { 255, 175, 0 };
 RGB white = { 255, 255, 255 };
 
-int minBrightness = 50;
-int maxBrightness = 255;
-int interval = 80;
+int minBrightness = 50 / 20;   // LEDs completely off would be 0
+int maxBrightness = 255 / 20;  // Maximum brightness on the LEDs would be 255
+int interval = 300;
 int stepSize = 2;
 
 
@@ -34,9 +35,33 @@ void setAllPixelsYellow() {
   }
 }
 
-
-
 void loop() {
+
+  setAllPixelsYellow();
+  pixels.setPixelColor(random(0, 6), white.r, white.g, white.b);
+  pixels.setPixelColor(random(7, 12), white.r, white.g, white.b);
+  pixels.setPixelColor(random(0, 6), coolYellow.r, coolYellow.g, coolYellow.b);
+  pixels.setPixelColor(random(7, 12), coolYellow.r, coolYellow.g, coolYellow.b);
+  pixels.show();
+  delay(interval);
+
+  // Fade from min brightness to max brightness
+  for (int brightness = minBrightness; brightness <= maxBrightness; brightness += stepSize) {
+    pixels.setBrightness(brightness);
+    pixels.show();
+    delay(interval);
+  };
+
+  // Then fade from max down to min again
+  for (int brightness = maxBrightness; brightness >= minBrightness; brightness -= stepSize) {
+    pixels.setBrightness(brightness);
+    pixels.show();
+    delay(interval);
+  };
+
+
+
+
 
   // for (int i = 0; i <=10; i++){
   //   pixels.setBrightness(10 * i);
@@ -44,67 +69,62 @@ void loop() {
   //   delay(100);
   // }
 
-  // For all pixels...
-  for (int i = 0; i < NUMPIXELS; i++) {
-
-    int randomWhiteLED1 = random(0, 6);
-    int randomWhiteLED2 = random(7, 12);
-
-
-    // Go from min brightness to max brightness
-    for (int brightness = minBrightness; brightness <= maxBrightness; brightness += stepSize) {
-      setAllPixelsYellow();
-      pixels.setPixelColor(randomWhiteLED1, white.r, white.g, white.b);
-      pixels.setPixelColor(randomWhiteLED2, white.r, white.g, white.b);
-
-      pixels.setBrightness(brightness);
-      pixels.show();
-      delay(interval);
-    }
-
-    // Then go from max brightness to min brightness
-    for (int brightness = maxBrightness; brightness > minBrightness; brightness -= stepSize) {
-      // setAllPixelsYellow();
-      pixels.setBrightness(brightness);
-      pixels.show();
-      delay(interval);
-    }
-
-    setAllPixelsYellow();
-
-
-    // pixels.setPixelColor(i - 4, coolWhite.r, coolWhite.g, coolWhite.b);
-    // pixels.setPixelColor(i - 1, warmYellow.r, warmYellow.g, warmYellow.b);
-    // pixels.setPixelColor(i + 4, warmYellow.r, warmYellow.g, warmYellow.b);
-
-    // pixels.setPixelColor(i - 5, coolWhite.r, coolWhite.g, coolWhite.b);
-    // pixels.setPixelColor(i, coolWhite.r, coolWhite.g, coolWhite.b);
-    // pixels.setPixelColor(i + 5, coolWhite.r, coolWhite.g, coolWhite.b);
-
-    // pixels.show();
-    // delay(interval);
-
-
-
-
-
-
-    // pixels.show();
-    // delay(interval);
-
-
-    // pixels.setPixelColor(i, myColour.r, myColour.g, myColour.b);
-    // pixels.show();
-    // delay(300);
-  }
-
+  // // CYCLE 1
+  // // For all pixels...
   // for (int i = 0; i < NUMPIXELS; i++) {
-  //   pixels.clear();
-  //   pixels.setPixelColor(i, pixels.Color(red.r, red.g, red.g));
-  //   pixels.setPixelColor(i+1, pixels.Color(green.r, green.g, green.b));
-  //   pixels.setPixelColor(i+2, pixels.Color(blue.r, blue.g, blue.b));
 
-  //   pixels.show();
-  //   delay(300);
+  //   int randomWhiteLED1 = random(0, 6);
+  //   int randomWhiteLED2 = random(7, 12);
+  //   setAllPixelsYellow();
+
+  //   // Go from min brightness to max brightness
+  //   for (int brightness = minBrightness; brightness <= maxBrightness; brightness += stepSize) {
+  //     // setAllPixelsYellow();
+
+  //     pixels.setPixelColor(randomWhiteLED1, white.r, white.g, white.b);
+  //     pixels.setPixelColor(randomWhiteLED2, white.r, white.g, white.b);
+
+  //     // pixels.setBrightness(brightness);
+  //     pixels.show();
+  //     delay(interval);
+  //   }
+
+  //   // Then go from max brightness to min brightness
+  //   for (int brightness = maxBrightness; brightness > minBrightness; brightness -= stepSize) {
+  //     // setAllPixelsYellow();
+  //     // pixels.setBrightness(brightness);
+  //     pixels.show();
+  //     delay(interval);
+  //   }
+  // }
+
+  // // CYCLE 2
+  // for (int i = 0; i < NUMPIXELS; i += 2) {
+
+  //   int randomWhiteLED1 = random(0, 6);
+  //   int randomWhiteLED2 = random(7, 12);
+  //   setAllPixelsYellow();
+
+
+  //   // Go from min brightness to max brightness
+  //   for (int brightness = minBrightness; brightness <= maxBrightness; brightness += stepSize) {
+  //     // setAllPixelsYellow();
+
+
+  //     // pixels.setPixelColor(randomWhiteLED1, white.r, white.g, white.b);
+  //     // pixels.setPixelColor(randomWhiteLED2, white.r, white.g, white.b);
+
+  //     pixels.setBrightness(brightness);
+  //     pixels.show();
+  //     delay(interval);
+  //   }
+
+  //   // Then go from max brightness to min brightness
+  //   for (int brightness = maxBrightness; brightness > minBrightness; brightness -= stepSize) {
+  //     // setAllPixelsYellow();
+  //     pixels.setBrightness(brightness);
+  //     pixels.show();
+  //     delay(interval);
+  //   }
   // }
 }
