d546eb5c6c32197ffebb79970bfc7cbed0b1f819
Ceri <cxv712@gmail.com>
2025-06-29 23:46:49 +0100

    Refactor to FastLED, one LED fading in and out yellow

--- blood-of-lathander-lights.ino
+++ blood-of-lathander-lights.ino
@@ -1,130 +1,45 @@
-#include <Adafruit_NeoPixel.h>
-#define PIN 3
-#define NUMPIXELS 13
+#include <FastLED.h>
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define NUM_LEDS 13
+#define DATA_PIN 3
 
-// Define a type and store some colours for my use
-struct RGB {
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
-};
+CRGB leds[NUM_LEDS];
 
-RGB warmYellow = { 255, 125, 0 };
-RGB coolYellow = { 255, 175, 0 };
-RGB white = { 255, 255, 255 };
-
-int minBrightness = 50 / 20;   // LEDs completely off would be 0
-int maxBrightness = 255 / 20;  // Maximum brightness on the LEDs would be 255
-int interval = 300;
-int stepSize = 2;
-
+// Warm yellow in HSV
+const CHSV warmYellow = CHSV(35, 255, 255);
+const CRGB white = CRGB::White;
 
 void setup() {
-
-  pixels.begin();
-  pixels.setBrightness(minBrightness);
-  setAllPixelsYellow();
-}
-
-void setAllPixelsYellow() {
-  for (int ledRef = 0; ledRef < NUMPIXELS; ledRef++) {
-    pixels.setPixelColor(ledRef, warmYellow.r, warmYellow.g, warmYellow.b);
-    pixels.show();
-  }
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.clear();
+  FastLED.show();
 }
 
 void loop() {
+  // start off with warm yellow
+  leds[0] = warmYellow;
+  FastLED.show();
 
-  setAllPixelsYellow();
-  pixels.setPixelColor(random(0, 6), white.r, white.g, white.b);
-  pixels.setPixelColor(random(7, 12), white.r, white.g, white.b);
-  pixels.setPixelColor(random(0, 6), coolYellow.r, coolYellow.g, coolYellow.b);
-  pixels.setPixelColor(random(7, 12), coolYellow.r, coolYellow.g, coolYellow.b);
-  pixels.show();
-  delay(interval);
+  delay(1000);
 
-  // Fade from min brightness to max brightness
-  for (int brightness = minBrightness; brightness <= maxBrightness; brightness += stepSize) {
-    pixels.setBrightness(brightness);
-    pixels.show();
-    delay(interval);
-  };
+  uint8_t minBrightness = 100;
+  uint8_t maxBrightness = 255;
 
-  // Then fade from max down to min again
-  for (int brightness = maxBrightness; brightness >= minBrightness; brightness -= stepSize) {
-    pixels.setBrightness(brightness);
-    pixels.show();
-    delay(interval);
-  };
+  // Fade out
+  for (int i = maxBrightness; i >= minBrightness; i--) {
+    leds[0] = CHSV(warmYellow.h, warmYellow.s, i);
+    FastLED.show();
+    delay(10);
+  }
 
+  delay(500);
 
+  // Fade in
+  for (int i = minBrightness; i <= maxBrightness; i++) {
+    leds[0] = CHSV(warmYellow.h, warmYellow.s, i);
+    FastLED.show();
+    delay(10);
+  }
 
-
-
-  // for (int i = 0; i <=10; i++){
-  //   pixels.setBrightness(10 * i);
-  //   pixels.show();
-  //   delay(100);
-  // }
-
-  // // CYCLE 1
-  // // For all pixels...
-  // for (int i = 0; i < NUMPIXELS; i++) {
-
-  //   int randomWhiteLED1 = random(0, 6);
-  //   int randomWhiteLED2 = random(7, 12);
-  //   setAllPixelsYellow();
-
-  //   // Go from min brightness to max brightness
-  //   for (int brightness = minBrightness; brightness <= maxBrightness; brightness += stepSize) {
-  //     // setAllPixelsYellow();
-
-  //     pixels.setPixelColor(randomWhiteLED1, white.r, white.g, white.b);
-  //     pixels.setPixelColor(randomWhiteLED2, white.r, white.g, white.b);
-
-  //     // pixels.setBrightness(brightness);
-  //     pixels.show();
-  //     delay(interval);
-  //   }
-
-  //   // Then go from max brightness to min brightness
-  //   for (int brightness = maxBrightness; brightness > minBrightness; brightness -= stepSize) {
-  //     // setAllPixelsYellow();
-  //     // pixels.setBrightness(brightness);
-  //     pixels.show();
-  //     delay(interval);
-  //   }
-  // }
-
-  // // CYCLE 2
-  // for (int i = 0; i < NUMPIXELS; i += 2) {
-
-  //   int randomWhiteLED1 = random(0, 6);
-  //   int randomWhiteLED2 = random(7, 12);
-  //   setAllPixelsYellow();
-
-
-  //   // Go from min brightness to max brightness
-  //   for (int brightness = minBrightness; brightness <= maxBrightness; brightness += stepSize) {
-  //     // setAllPixelsYellow();
-
-
-  //     // pixels.setPixelColor(randomWhiteLED1, white.r, white.g, white.b);
-  //     // pixels.setPixelColor(randomWhiteLED2, white.r, white.g, white.b);
-
-  //     pixels.setBrightness(brightness);
-  //     pixels.show();
-  //     delay(interval);
-  //   }
-
-  //   // Then go from max brightness to min brightness
-  //   for (int brightness = maxBrightness; brightness > minBrightness; brightness -= stepSize) {
-  //     // setAllPixelsYellow();
-  //     pixels.setBrightness(brightness);
-  //     pixels.show();
-  //     delay(interval);
-  //   }
-  // }
+  delay(500);
 }
