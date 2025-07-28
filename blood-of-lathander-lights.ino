802519e41b666c95c54b735a8297c7d465b2bb45
Ceri <cxv712@gmail.com>
2025-07-28 22:33:18 +0100

    Better spread of white LEDs across hemispheres

--- blood-of-lathander-lights.ino
+++ blood-of-lathander-lights.ino
@@ -7,9 +7,12 @@
 // For my prop, there are 13 spikes, plus 3 more LEDs - 2 in the main sphere and one in the pummel.
 #define SPIKE_LEDS 13
 
-// The number of the LED that's in the pummel.
-// We'll do a separate animation for this.
-#define PUMMEL_LED 16
+const int TOP_SPIKE = 0;
+const int UPPER_HEMISPHERE_SPIKES[] = {1, 2, 4, 5};
+const int SIDE_SPIKES[] = {3, 6, 8, 11};
+const int LOWER_HEMISPHERE_SPIKES[] = {7, 9, 10, 12};
+const int INTERNAL_LEDS[] = {13, 14};
+const int PUMMEL_LED = 15;
 
 CRGB leds[NUM_LEDS];
 
@@ -17,6 +20,7 @@ const CHSV warmYellow = CHSV(35, 255, 255);
 const CHSV white = CHSV(0, 0, 100);
 
 int dimBrightness = 50; // The brightness for the dimmest point, from a max of 255
+int maxBrightness = 255;
 int fadeSpeed = 2.5; // How fast the LEDs fade in and out. Higher numbers will be slower
 
 
@@ -28,37 +32,37 @@ void setup() {
   FastLED.show();
 }
 
-void fadeIn(int led, CHSV colour, int from, int to) {
-  for (int i = from; i <= to; i++) {
+void fadeIn(int led, CHSV colour) {
+  for (int i = dimBrightness; i <= maxBrightness; i++) {
     leds[led] = CHSV(colour.h, colour.s, i);
     FastLED.show();
     delay(fadeSpeed);
   }
   // Ensure final brightness
-  leds[led] = CHSV(colour.h, colour.s, to);
+  leds[led] = CHSV(colour.h, colour.s, maxBrightness);
   FastLED.show();
 }
 
-void fadeOut(int led, CHSV colour, int from, int to) {
-  for (int i = from; i >= to; i--) {
+void fadeOut(int led, CHSV colour) {
+  for (int i = maxBrightness; i >= dimBrightness; i--) {
     leds[led] = CHSV(colour.h, colour.s, i);
     FastLED.show();
     delay(fadeSpeed);
   }
   // Ensure final brightness
-  leds[led] = CHSV(colour.h, colour.s, to);
+  leds[led] = CHSV(colour.h, colour.s, dimBrightness);
   FastLED.show();
 }
 
 void fadeAllInOut(CHSV colour) {
   // Fade all LEDs in and out yellow for fun
-  for (int i = 255; i > 50; i--) {
+  for (int i = maxBrightness; i > dimBrightness; i--) {
     fill_solid(leds, NUM_LEDS, CHSV(colour.h, colour.s, i));
     FastLED.show();
     delay(10);
   }
 
-  for (int i = 50; i <= 255; i++) {
+  for (int i = dimBrightness; i <= maxBrightness; i++) {
     fill_solid(leds, NUM_LEDS, CHSV(colour.h, colour.s, i));
     FastLED.show();
     delay(10);
@@ -74,11 +78,9 @@ void loop() {
   // fadeAllInOut(warmYellow);
 
 
-
-
-  int whiteLED1 = random(0, SPIKE_LEDS / 3);
-  int whiteLED2 = random(SPIKE_LEDS / 3, SPIKE_LEDS / 3 * 2);
-  int whiteLED3 = random(SPIKE_LEDS / 3 * 2, SPIKE_LEDS);
+  int whiteLED1 = UPPER_HEMISPHERE_SPIKES[random(0, 3)];
+  int whiteLED2 = LOWER_HEMISPHERE_SPIKES[random(0, 3)];
+  int whiteLED3 = SIDE_SPIKES[random(0, 3)];
 
   // Choose a random LED to dim which isn't one of the white ones
   int dimLED1 = random(0, SPIKE_LEDS / 3);
@@ -98,12 +100,12 @@ void loop() {
     dimLED3 = random(SPIKE_LEDS / 3 * 2, SPIKE_LEDS);
   }
 
-  fadeOut(whiteLED1, warmYellow, 255, dimBrightness);  // Keep our whiteLED1 yellow, but fade to nothing
-  fadeOut(dimLED1, warmYellow, 255, dimBrightness);    // Make dimLED1 dim
-  fadeOut(whiteLED2, warmYellow, 255, dimBrightness);  // Keep our whiteLED2 yellow, but fade to nothing
-  fadeOut(dimLED2, warmYellow, 255, dimBrightness);    // Make dimLED1 dim
-  fadeOut(whiteLED3, warmYellow, 255, dimBrightness);  // Keep our whiteLED3 yellow, but fade to nothing
-  fadeOut(dimLED3, warmYellow, 255, dimBrightness);    // Make dimLED3 dim
+  fadeOut(whiteLED1, warmYellow);  // Keep our whiteLED1 yellow, but fade to nothing
+  fadeOut(dimLED1, warmYellow);    // Make dimLED1 dim
+  fadeOut(whiteLED2, warmYellow);  // Keep our whiteLED2 yellow, but fade to nothing
+  fadeOut(dimLED2, warmYellow);    // Make dimLED1 dim
+  fadeOut(whiteLED3, warmYellow);  // Keep our whiteLED3 yellow, but fade to nothing
+  fadeOut(dimLED3, warmYellow);    // Make dimLED3 dim
 
 
   // Current state:
@@ -116,15 +118,15 @@ void loop() {
 
   // -------
 
-  fadeIn(whiteLED1, white, dimBrightness, 255);  // fade whiteLED1 to full white brightness
-  fadeIn(whiteLED2, white, dimBrightness, 255);  // fade whiteLED2 to full white brightness
-  fadeIn(whiteLED3, white, dimBrightness, 255);  // fade whiteLED3 to full white brightness
+  fadeIn(whiteLED1, white);  // fade whiteLED1 to full white brightness
+  fadeIn(whiteLED2, white);  // fade whiteLED2 to full white brightness
+  fadeIn(whiteLED3, white);  // fade whiteLED3 to full white brightness
 
-  fadeIn(dimLED1, warmYellow, dimBrightness, 255);  // bring dim leds back to full brightness
-  fadeIn(dimLED2, warmYellow, dimBrightness, 255);  // bring dim leds back to full brightness
-  fadeIn(dimLED3, warmYellow, dimBrightness, 255);  // bring dim leds back to full brightness
+  fadeIn(dimLED1, warmYellow);  // bring dim leds back to full brightness
+  fadeIn(dimLED2, warmYellow);  // bring dim leds back to full brightness
+  fadeIn(dimLED3, warmYellow);  // bring dim leds back to full brightness
 
-  fadeIn(PUMMEL_LED, warmYellow, dimBrightness, 255);
+  fadeIn(PUMMEL_LED, warmYellow);
 
 
   // Current state:
@@ -138,15 +140,15 @@ void loop() {
   // -------
 
   // Dim and re-brighten dim leds
-  fadeOut(dimLED1, warmYellow, 255, dimBrightness);  // dimLED1 dim again
-  fadeOut(dimLED2, warmYellow, 255, dimBrightness);  // dimLED2 dim again
-  fadeOut(dimLED3, warmYellow, 255, dimBrightness);  // dimLED3 dim again
-  fadeOut(PUMMEL_LED, warmYellow, 255, dimBrightness);
+  fadeOut(dimLED1, warmYellow);  // dimLED1 dim again
+  fadeOut(dimLED2, warmYellow);  // dimLED2 dim again
+  fadeOut(dimLED3, warmYellow);  // dimLED3 dim again
+  fadeOut(PUMMEL_LED, warmYellow);
 
-  fadeIn(dimLED1, warmYellow, dimBrightness, 255);  // bring dim leds back to full brightness
-  fadeIn(dimLED2, warmYellow, dimBrightness, 255);  // bring dim leds back to full brightness
-  fadeIn(dimLED3, warmYellow, dimBrightness, 255);  // bring dim leds back to full brightness
-  fadeIn(PUMMEL_LED, warmYellow, dimBrightness, 255);
+  fadeIn(dimLED1, warmYellow);  // bring dim leds back to full brightness
+  fadeIn(dimLED2, warmYellow);  // bring dim leds back to full brightness
+  fadeIn(dimLED3, warmYellow);  // bring dim leds back to full brightness
+  fadeIn(PUMMEL_LED, warmYellow);
 
 
 
@@ -160,10 +162,10 @@ void loop() {
 
   // -------
 
-  fadeOut(whiteLED1, white, 255, dimBrightness);  // fade whiteLED1 to nothing
-  fadeOut(whiteLED2, white, 255, dimBrightness);  // fade whiteLED2 to nothing
-  fadeOut(whiteLED3, white, 255, dimBrightness);  // fade whiteLED2 to nothing
-  fadeOut(PUMMEL_LED, warmYellow, 255, dimBrightness);
+  fadeOut(whiteLED1, white);  // fade whiteLED1 to nothing
+  fadeOut(whiteLED2, white);  // fade whiteLED2 to nothing
+  fadeOut(whiteLED3, white);  // fade whiteLED2 to nothing
+  fadeOut(PUMMEL_LED, warmYellow);
 
   // Current state:
   // dimLED1: max brightness
@@ -175,10 +177,10 @@ void loop() {
 
   // -------
 
-  fadeIn(whiteLED1, warmYellow, dimBrightness, 255);
-  fadeIn(whiteLED2, warmYellow, dimBrightness, 255);
-  fadeIn(whiteLED3, warmYellow, dimBrightness, 255);
-  fadeIn(PUMMEL_LED, warmYellow, dimBrightness, 255);
+  fadeIn(whiteLED1, warmYellow);
+  fadeIn(whiteLED2, warmYellow);
+  fadeIn(whiteLED3, warmYellow);
+  fadeIn(PUMMEL_LED, warmYellow);
 
   // Current state:
   // dimLED1: max brightness
