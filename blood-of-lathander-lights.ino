ac43561c8156f6c0781c81829e40ca08333e89cc
Ceri <cxv712@gmail.com>
2025-07-28 22:47:50 +0100

    Add another white led
    After some real life testing where I discovered my battery lasts way longer than I need it to, I decided I could afford to add some more white (which is typically more draining on the battery, but looks good)

--- blood-of-lathander-lights.ino
+++ blood-of-lathander-lights.ino
@@ -75,28 +75,28 @@ void loop() {
 
   fadeAllInOut(warmYellow);
   fadeAllInOut(warmYellow);
-  // fadeAllInOut(warmYellow);
 
 
-  int whiteLED1 = UPPER_HEMISPHERE_SPIKES[random(0, 3)];
-  int whiteLED2 = LOWER_HEMISPHERE_SPIKES[random(0, 3)];
-  int whiteLED3 = SIDE_SPIKES[random(0, 3)];
+  int whiteLED1 = UPPER_HEMISPHERE_SPIKES[random(0, 2)];
+  int whiteLED2 = LOWER_HEMISPHERE_SPIKES[random(0, 2)];
+  int whiteLED3 = UPPER_HEMISPHERE_SPIKES[random(2, 4)];
+  int whiteLED4 = UPPER_HEMISPHERE_SPIKES[random(2, 4)];
 
   // Choose a random LED to dim which isn't one of the white ones
   int dimLED1 = random(0, SPIKE_LEDS / 3);
-  while (dimLED1 == whiteLED1 || dimLED1 == whiteLED2 || dimLED1 == whiteLED3) {
+  while (dimLED1 == whiteLED1 || dimLED1 == whiteLED2 || dimLED1 == whiteLED3 || dimLED1 == whiteLED4) {
     dimLED1 = random(0, SPIKE_LEDS / 3);
   }
 
   // Choose another random LED to dim which isn't one of the white ones
   int dimLED2 = random(SPIKE_LEDS / 3, SPIKE_LEDS / 3 * 2);
-  while (dimLED2 == whiteLED1 || dimLED2 == whiteLED2 || dimLED2 == whiteLED3 || dimLED2 == dimLED1) {
+  while (dimLED2 == whiteLED1 || dimLED2 == whiteLED2 || dimLED2 == whiteLED3 || dimLED2 == dimLED1 || dimLED2 == whiteLED4) {
     dimLED2 = random(SPIKE_LEDS / 3, SPIKE_LEDS / 3 * 2);
   }
 
   // Choose another random LED to dim which isn't one of the white ones
   int dimLED3 = random(SPIKE_LEDS / 3 * 2, SPIKE_LEDS);
-  while (dimLED3 == whiteLED1 || dimLED3 == whiteLED2 || dimLED3 == whiteLED3 || dimLED3 == dimLED1 || dimLED3 == dimLED2) {
+  while (dimLED3 == whiteLED1 || dimLED3 == whiteLED2 || dimLED3 == whiteLED3 || dimLED3 == dimLED1 || dimLED3 == dimLED2 || dimLED3 == whiteLED4) {
     dimLED3 = random(SPIKE_LEDS / 3 * 2, SPIKE_LEDS);
   }
 
@@ -106,90 +106,81 @@ void loop() {
   fadeOut(dimLED2, warmYellow);    // Make dimLED1 dim
   fadeOut(whiteLED3, warmYellow);  // Keep our whiteLED3 yellow, but fade to nothing
   fadeOut(dimLED3, warmYellow);    // Make dimLED3 dim
+  fadeOut(whiteLED4, warmYellow);  // Keep our whiteLED3 yellow, but fade to nothing
+
 
 
   // Current state:
-  // dimLED1: dim
-  // dimLED2: dim
-  // dimLED3: dim
-  // whiteLED1: off
-  // whiteLED2: off
-  // whiteLED3: off
+  // dim leds - dim
+  // white leds - dim
 
   // -------
 
-  fadeIn(whiteLED1, white);  // fade whiteLED1 to full white brightness
-  fadeIn(whiteLED2, white);  // fade whiteLED2 to full white brightness
-  fadeIn(whiteLED3, white);  // fade whiteLED3 to full white brightness
+  // Fade white LEDs to full white brightness
+  fadeIn(whiteLED1, white);
+  fadeIn(whiteLED2, white);
+  fadeIn(whiteLED3, white);
+  fadeIn(whiteLED4, white);
 
-  fadeIn(dimLED1, warmYellow);  // bring dim leds back to full brightness
-  fadeIn(dimLED2, warmYellow);  // bring dim leds back to full brightness
-  fadeIn(dimLED3, warmYellow);  // bring dim leds back to full brightness
+  // Bring dim LEDs back to full brightness
+  fadeIn(dimLED1, warmYellow);
+  fadeIn(dimLED2, warmYellow);
+  fadeIn(dimLED3, warmYellow);
 
   fadeIn(PUMMEL_LED, warmYellow);
 
 
   // Current state:
-  // dimLED1: max brightness
-  // dimLED1: max brightness
-  // dimLED3: max brightness
-  // whiteLED1: max brightness (white)
-  // whiteLED2: max brightness (white)
-  // whiteLED3: max brightness (white)
+  // dim leds - max brightness
+  // white leds - max brightness (white)
+
 
   // -------
 
   // Dim and re-brighten dim leds
-  fadeOut(dimLED1, warmYellow);  // dimLED1 dim again
-  fadeOut(dimLED2, warmYellow);  // dimLED2 dim again
-  fadeOut(dimLED3, warmYellow);  // dimLED3 dim again
+  fadeOut(dimLED1, warmYellow);
+  fadeOut(dimLED2, warmYellow);
+  fadeOut(dimLED3, warmYellow);
   fadeOut(PUMMEL_LED, warmYellow);
 
-  fadeIn(dimLED1, warmYellow);  // bring dim leds back to full brightness
-  fadeIn(dimLED2, warmYellow);  // bring dim leds back to full brightness
-  fadeIn(dimLED3, warmYellow);  // bring dim leds back to full brightness
+  fadeIn(dimLED1, warmYellow);
+  fadeIn(dimLED2, warmYellow);
+  fadeIn(dimLED3, warmYellow);
   fadeIn(PUMMEL_LED, warmYellow);
 
 
-
   // Current state:
-  // dimLED1: max brightness
-  // dimLED2: max brightness
-  // dimLED3: max brightness
-  // whiteLED1: max brightness (white)
-  // whiteLED2: max brightness (white)
-  // whiteLED3: max brightness (white)
+  // dim leds - max brightness
+  // white leds - max brightness (white)
+
 
   // -------
 
-  fadeOut(whiteLED1, white);  // fade whiteLED1 to nothing
-  fadeOut(whiteLED2, white);  // fade whiteLED2 to nothing
-  fadeOut(whiteLED3, white);  // fade whiteLED2 to nothing
+  // Fade white LEDs to nothing
+  fadeOut(whiteLED1, white);
+  fadeOut(whiteLED2, white);
+  fadeOut(whiteLED3, white);
+  fadeOut(whiteLED4, white);
+
   fadeOut(PUMMEL_LED, warmYellow);
 
   // Current state:
-  // dimLED1: max brightness
-  // dimLED2: max brightness
-  // dimLED3: max brightness
-  // whiteLED1: off
-  // whiteLED2: off
-  // whiteLED3: off
+  // dim leds - max brightness
+  // white leds - dim
 
   // -------
 
   fadeIn(whiteLED1, warmYellow);
   fadeIn(whiteLED2, warmYellow);
   fadeIn(whiteLED3, warmYellow);
+  fadeIn(whiteLED4, warmYellow);
+
   fadeIn(PUMMEL_LED, warmYellow);
 
   // Current state:
-  // dimLED1: max brightness
-  // dimLED2: max brightness
-  // dimLED3: max brightness
-  // whiteLED1: max brightness (yellow)
-  // whiteLED2: max brightness (yellow)
-  // whiteLED3: max brightness (yellow)
-  // }
+  // dim leds - max brightness
+  // white leds - max brightness (yellow)
+
 
   delay(10);
 }
